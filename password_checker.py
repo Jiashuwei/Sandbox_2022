a7a5d8389f699a1dc595d2ad251765c723e15f73
Jiashu Wei <Jiashu.Wei@my.jcu.edu.au>
2022-03-30 22:07:27 +0800

    Modify function to add criteria for valid password

--- password_checker.py
+++ password_checker.py
@@ -1,23 +1,15 @@
 """
-Description: A simple password checker
+Description: A password checking funtion
 
 Name: Jiashu Wei
 """
-
-
 def is_valid_password(text):
-    """Check whether a given text has the correct password format"""
-    return 8 <= len(text) <= 20
+    return 8 <= len(text) <= 20 and "#" in text
 
 def main():
-    """Start program"""
     new_password = "hello world"
     print(f"{new_password} is a valid password? {is_valid_password(new_password)}")
 
 
 if __name__ == '__mian__':
     main()
-
-
-
-
