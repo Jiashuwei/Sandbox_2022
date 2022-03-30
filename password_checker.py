13ff487aa3a46effbfc728a3f13d9f8fd052785c
Jiashu Wei <Jiashu.Wei@my.jcu.edu.au>
2022-03-30 21:15:16 +0800

    Add docstring to password checker

--- password_checker.py
+++ password_checker.py
@@ -1,11 +1,23 @@
+"""
+Description: A simple password checker
+
+Name: Jiashu Wei
+"""
+
 
 def is_valid_password(text):
+    """Check whether a given text has the correct password format"""
     return 8 <= len(text) <= 20
 
 def main():
+    """Start program"""
     new_password = "hello world"
     print(f"{new_password} is a valid password? {is_valid_password(new_password)}")
 
 
 if __name__ == '__mian__':
     main()
+
+
+
+
